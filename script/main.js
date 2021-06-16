c6e318c4e47ed1e4d516a45d08c97961a083608b
Jacob Hytter <jacob.hytter@hiq.se>
2021-06-16 16:14:38 +0200

    Mother of all comits

---
+++ script/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  var isMobile = false; //initiate as false
+  $(window).on('load scroll resize', function () {
+    var docHeight = $(document).height();
+    var windowPos = $(window).scrollTop();
+    var windowHeight = $(window).height();
+    var windowWidth = $(window).width();
+    var completion = windowPos / (docHeight - windowHeight);
+
+    if (docHeight <= windowHeight) {
+      $('#progress').width(windowWidth);
+    } else {
+      $('#progress').width(completion * windowWidth);
+    }
+  });
+});
